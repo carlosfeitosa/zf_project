755f500b5e15512e2412b9e3bc3c359cd469da29
joao.vasconcelos <joao.vasconcelos@19d60574-38d4-4ac3-a504-0da25a219391>
2010-01-07 21:23:42 +0000

    adicionado o  botão para fechar o alertDialog do DOJO.

--- rochedo/zendstudio/zf_project/public/js/default_scripts.js
+++ rochedo/zendstudio/zf_project/public/js/default_scripts.js
@@ -5,19 +5,26 @@ dojo.require("dijit.Form");
 function validateForm(formid) {
 var form = dijit.byId(formid);
 if (!form.validate()) {
-	dialogAlert("Atenção!", "Preencha todos os campos antes de continuar.");
+	showDialogAlert("dialogCadastroNovoUsuario", "Atenção!", "Preencha todos os campos antes de continuar.");
 return false;
 }
 
 return true;
 }
 
-function dialogAlert(txtTitle, txtContent)
+function showDialogAlert(txtDialogId, txtTitle, txtContent)
 {
-	var thisdialog = new dijit.Dialog({ title: txtTitle, content: txtContent });
+	
+	var botaoFechar = "<br><br><center><button dojoType='dijit.form.Button' type='submit' onclick='hideDialog('"+ txtDialogId +"')'>Ok</button></center>";
+		
+	var thisdialog = new dijit.Dialog({ title: txtTitle, content: txtContent+botaoFechar});
 	dojo.body().appendChild(thisdialog.domNode);
 	thisdialog.startup();
 	thisdialog.show();
 }
 
-
+function hideDialog(dialogId){
+    
+    var dlg = dijit.byId(dialogId);
+    dlg.hide();
+}
