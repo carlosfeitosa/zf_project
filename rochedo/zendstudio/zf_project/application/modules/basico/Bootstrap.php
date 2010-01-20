9a23797f7579ea03f0efad7e82abea9402710bcb
joao.vasconcelos <joao.vasconcelos@19d60574-38d4-4ac3-a504-0da25a219391>
2010-01-20 13:58:07 +0000

    Adição/Edição de Comentários.

--- rochedo/zendstudio/zf_project/application/modules/basico/Bootstrap.php
+++ rochedo/zendstudio/zf_project/application/modules/basico/Bootstrap.php
@@ -1,7 +1,14 @@
 <?php
-
+/**
+ * Bootstrap do módulo Basico.
+ *
+ */
 class Basico_Bootstrap extends Zend_Application_Module_Bootstrap
 {
+	/**
+	 * Inicializa Autoload.
+	 * @return Zend_Application_Module_Autoloader
+	 */
     protected function _initAutoload()
     {   
         $autoloader = new Zend_Application_Module_Autoloader(array(
