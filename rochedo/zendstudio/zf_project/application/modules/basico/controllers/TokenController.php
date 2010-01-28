446708a444b1600227b9c6e12d2f67ab2097716d
joao.vasconcelos <joao.vasconcelos@19d60574-38d4-4ac3-a504-0da25a219391>
2010-01-28 13:42:08 +0000

    adicao dos metodos init e construct nos controladores Token e CategoriaChaveEstrangeira.

--- rochedo/zendstudio/zf_project/application/modules/basico/controllers/TokenController.php
+++ rochedo/zendstudio/zf_project/application/modules/basico/controllers/TokenController.php
@@ -1,5 +1,34 @@
 <?php
 class Basico_TokenController
 {
-		
+/**
+	 * @var Basico_TokenController
+	 */
+	static private $singleton;
+	
+	/**
+	 * @var Basico_Model_Token
+	 */
+	private $token;
+	
+    /**
+     * Construtor do Controller
+     * @return void
+     */
+	private function __construct()
+	{
+		$this->token = new Basico_Model_Token();
+	}
+	
+	/**
+	 * Inicializa o controlador Basico_TokenController
+	 * @return Basico_TokenController
+	 */
+	public function init()
+	{
+		if(self::$singleton == NULL){
+			self::$singleton = new Basico_TokenController();
+		}
+		return self::$singleton;
+	}	
 }
