a98d1f2d73f8496734bc7c15f1ad82cb852f941f
joao.vasconcelos <joao.vasconcelos@19d60574-38d4-4ac3-a504-0da25a219391>
2010-06-30 16:49:05 +0000

    remoção de arquivos inuteis do modulo GC.

--- rochedo/zendstudio/zf_project/application/modules/basico/forms/InformacoesBancarias.php
+++
@@ -1,8 +0,0 @@
-<?php 
-      		  $labelSubformTabTitleInformacoesBancarias = $this->getView()->tradutor(SUBFORM_TABTITLE_INFORMACOES_BANCARIAS, DEFAULT_USER_LANGUAGE);	
-              $informacoesBancariasForm = new Zend_Dojo_Form_SubForm();
-              $informacoesBancariasForm->setName('CadastrarInformacoesBancarias');
-              $informacoesBancariasForm->setAttribs(array(
-                  'legend' => $labelSubformTabTitleInformacoesBancarias,
-              ));
-              $this->addSubForm($informacoesBancariasForm, 'CadastrarInformacoesBancarias');
--- rochedo/zendstudio/zf_project/application/modules/basico/forms/Perfil.php
+++
@@ -1,7 +0,0 @@
-<?php 		  $labelSubformTabTitlePerfil = $this->getView()->tradutor(SUBFORM_TABTITLE_PERFIL, DEFAULT_USER_LANGUAGE);	
-              $perfilForm = new Zend_Dojo_Form_SubForm();
-              $perfilForm->setName('CadastrarPerfil');
-              $perfilForm->setAttribs(array(
-                  'legend' => $labelSubformTabTitlePerfil,
-              ));
-			  $this->addSubForm($perfilForm, 'CadastrarPerfil'); 	
