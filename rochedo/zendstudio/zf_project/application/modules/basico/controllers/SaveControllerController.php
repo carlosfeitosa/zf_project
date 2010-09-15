43bc62e4f9b5a95c36ed7ee01889b759b370d254
carlos.feitosa <carlos.feitosa@19d60574-38d4-4ac3-a504-0da25a219391>
2010-09-15 18:46:52 +0000

    sprint 0006: - controlador Save, com versionamento e atualizacao de versao de tupla.

--- rochedo/zendstudio/zf_project/application/modules/basico/controllers/SaveControllerController.php
+++ rochedo/zendstudio/zf_project/application/modules/basico/controllers/SaveControllerController.php
@@ -14,10 +14,17 @@ class Basico_SaveControllerController
 		// recuperando o numero da ultima versao
 		$ultimaVersao = Basico_Model_CVC::retornaUltimaVersao($mixed, true);
 		
-		// versionando objeto
-		$versaoVersionamento = Basico_Model_CVC::versionar($mixed);
+		// verificando se o objeto deve ser versionado ou ter sua ultima versao atualizada apenas
+		if (false !== self::isInatualizarVersaoList($mixed)) {
+			// versionando objeto
+			$versaoVersionamento = Basico_Model_CVC::versionar($mixed);
+		}
+		else {
+			// atualizando o objeto
+			$versaoVersionamento = Basico_Model_CVC::atualizaVersao($mixed);
+		}
 		
-		// verificando se a versao eh a mesma
+		// verificando se houve atualizacao da versao
 		if ($ultimaVersao !== $versaoVersionamento) {
 			$mixed->save();
 			return true;
@@ -25,5 +32,14 @@ class Basico_SaveControllerController
 		else {
 			return false;
 		}
+	}
+	
+	static private function isInatualizarVersaoList($mixed)
+	{
+		// inicializando array contendo o nome das tabelas que devem atualizar a versao apenas
+		$arrayAtualizarList = array();
+		
+		// retornando resultado da pesquisa da tabela relacionada ao objeto dentro do array de tabelas que devem atualizar a versao apenas
+		return (false !== array_search(Basico_Model_Util::retornaTableNameObjeto($mixed), $arrayAtualizarList));
 	}
 }
