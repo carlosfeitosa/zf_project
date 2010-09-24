23a8c1f8c036fad765c56b37dd6f6d7cf628e91f
carlos.feitosa <carlos.feitosa@19d60574-38d4-4ac3-a504-0da25a219391>
2010-09-24 12:54:31 +0000

    correcao da ordem de drop das funcoes do script do sprint 0006. estes drops foram incorporados ao script do sprint 0001 por questoes de dependencia

---
+++ rochedo/zendstudio/zf_project/scripts/db/mssql/schema/sprint_0005_create_schema.sql
@@ -0,0 +1,10 @@
+/*
+* SCRIPT DE CRIACAO DAS TABELAS DO SPRINT 0005
+* 
+* versao: 1.0 (MSSQL 2000)
+* por: CARLOS FEITOSA (carlos.feitosa@rochedoproject.com)
+* criacao: 05/07/2010
+*/
+
+/* CRIACAO DAS TABELAS */
+
--- rochedo/zendstudio/zf_project/scripts/db/mssql/schema/sprint_0006_drop_schema.sql
+++ rochedo/zendstudio/zf_project/scripts/db/mssql/schema/sprint_0006_drop_schema.sql
@@ -4,8 +4,10 @@
 * versao: 1.0 (MSSQL 2000)
 * por: CARLOS FEITOSA (carlos.feitosa@rochedoproject.com)
 * criacao: 10/09/2010
+* ultimas modificacoes: 
+* 						- 24/09/2010 - transferencia dos drops das funcoes criadas no
+* 									   create schema do sprint 0006 para o drop
+* 									   do script 0001
 */
 
 drop table cvc;
-drop function fn_CheckCategoriaChaveEstrangeiraCategoriaExists;
-drop function fn_CheckCategoriaCVC;
