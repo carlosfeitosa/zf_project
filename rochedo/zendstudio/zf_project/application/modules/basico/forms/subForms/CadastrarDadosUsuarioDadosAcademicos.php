de9b1ffc79cd1e2eb812f3acd6d10299ef67658d
adriano.lemos <adriano.lemos@19d60574-38d4-4ac3-a504-0da25a219391>
2010-09-30 20:32:58 +0000

    Sprint 0003.2 - Dados Acadêmicos - Criação de subformulários de Dados Acadêmicos - Criação de subformulários de Dados Acadêmicos Maior Titulação

---
+++ rochedo/zendstudio/zf_project/application/modules/basico/forms/subForms/CadastrarDadosUsuarioDadosAcademicos.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* Rochedo Framework
+*
+* Formulário gerado automáticamente pelo Gerador rochedo
+* em: 30/09/2010 15:32:42
+*
+* LICENÇA DE USO
+*
+* (implementar licença)
+*
+*
+* @category   RochedoProject
+* @package    BASICO
+* @copyright  Copyright (c) 2010 Rochedo Project. (http://www.rochedoproject.com)
+* @license    (implementar)
+* @version    1: 27/09/2010 10:30:06
+*/
+    $basicoCadastrarDadosUsuarioDadosAcademicosSubForm = new Zend_Dojo_Form_SubForm();
+
+    // Inicializando o sub-formulário.
+    $basicoCadastrarDadosUsuarioDadosAcademicosSubForm->setName('CadastrarDadosUsuarioDadosAcademicos');
+    $basicoCadastrarDadosUsuarioDadosAcademicosSubForm->setMethod('post');
+    $basicoCadastrarDadosUsuarioDadosAcademicosSubForm->addAttribs(array('dijitParams' => array('title' => $this->getView()->tradutor(SUBFORM_TABTITLE_DADOS_ACADEMICOS)),'onSubmit'=>"loading();return(validateForm('CadastrarDadosUsuarioDadosAcademicos'))"));
+    $basicoCadastrarDadosUsuarioDadosAcademicosSubForm->setDecorators(array('FormElements',
+                array('TabContainer', array('id' => 'TabContainer', 'style' => 'width: 850px; height: 430px; position: relative; z-index: 3;',
+                      'dijitParams' => array('tabPosition' => 'top'),)),));
+
+    $this->addSubForm($basicoCadastrarDadosUsuarioDadosAcademicosSubForm, 'CadastrarDadosUsuarioDadosAcademicos');
+?>
