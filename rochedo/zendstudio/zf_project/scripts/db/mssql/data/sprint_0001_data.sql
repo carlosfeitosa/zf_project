e7e7e61c66dcf056a7b57eda5701a3b37b2be8fe
carlos.feitosa <carlos.feitosa@19d60574-38d4-4ac3-a504-0da25a219391>
2010-10-01 14:56:57 +0000

    sprint 0001: - correcao do texto da mensagem de confirmacao de cadastro e reenvio de confirmacao de cadastro.

--- rochedo/zendstudio/zf_project/scripts/db/mssql/data/sprint_0001_data.sql
+++ rochedo/zendstudio/zf_project/scripts/db/mssql/data/sprint_0001_data.sql
@@ -86,6 +86,7 @@ INSERT INTO mensagem (remetente, destinatarios, assunto, mensagem, id_categoria,
 SELECT 'SYSTEM_STARTUP' AS remetente, 'SYSTEM_STARTUP' AS destinatarios, 'Confirmação de Registro' AS assunto,
 'Prezado(a) sr.(a) [nome_usuario],
 
+
 Recentemente recebemos uma solicitação de registro em nosso sistema, relacionado ao seu endereço de e-mail a qual esta mensagem foi enviada.
 
 Caso você tenha feito esta solicitação, continue o seu registro clicando no link abaixo, ou copie/cole o endereço em seu navegador de preferência: 
@@ -104,6 +105,7 @@ AND c.nome = 'SISTEMA_MENSAGEM_EMAIL_TEMPLATE_VALIDACAO_USUARIO_PLAINTEXT';
 INSERT INTO mensagem (remetente, destinatarios, assunto, mensagem, id_categoria, datahora_mensagem, rowinfo)
 SELECT 'SYSTEM_STARTUP' AS remetente, 'SYSTEM_STARTUP' AS destinatarios, 'Reenvio de confirmação de Registro' AS assunto,
 'Prezado(a) sr.(a) [nome_usuario],
+
 
 Recentemente recebemos uma solicitação de registro em nosso sistema, relacionado ao seu endereço de e-mail a qual esta mensagem foi enviada.
 
