0ea11e73fd85684eed397e813bdc1ab9d75a56fd
adriano.lemos <adriano.lemos@19d60574-38d4-4ac3-a504-0da25a219391>
2010-10-06 16:53:07 +0000

    Sprint 0003.2 - Dados Acadêmicos -Criação de SubFormulário de Dados Acadêmicos

--- rochedo/zendstudio/zf_project/application/modules/basico/forms/subForms/CadastrarDadosUsuarioDadosAcademicos.php
+++ rochedo/zendstudio/zf_project/application/modules/basico/forms/subForms/CadastrarDadosUsuarioDadosAcademicos.php
@@ -3,7 +3,7 @@
 * Rochedo Framework
 *
 * Formulário gerado automáticamente pelo Gerador rochedo
-* em: 04/10/2010 16:22:27
+* em: 06/10/2010 13:46:15
 *
 * LICENÇA DE USO
 *
@@ -14,21 +14,22 @@
 * @package    BASICO
 * @copyright  Copyright (c) 2010 Rochedo Project. (http://www.rochedoproject.com)
 * @license    (implementar)
-* @version    1: 04/10/2010 14:13:18
+* @version    1: 04/10/2010 15:58:47
 */
     $basicoCadastrarDadosUsuarioDadosAcademicosSubForm = new Zend_Dojo_Form_SubForm();
 
     // Inicializando o sub-formulário.
     $basicoCadastrarDadosUsuarioDadosAcademicosSubForm->setName('CadastrarDadosUsuarioDadosAcademicos');
+    $basicoCadastrarDadosUsuarioDadosAcademicosSubForm->setMethod('post');
     $basicoCadastrarDadosUsuarioDadosAcademicosSubForm->addAttribs(array('dijitParams' => array('title' => $this->getView()->tradutor('SUBFORM_TABTITLE_DADOS_ACADEMICOS'))));
 
     // Criando array de elementos.
     $elements = array();
 
-    $basicoCadastrarUsuarioNaoValidadoSubFormDOJO = new Basico_Form_CadastrarUsuarioNaoValidado();
-    $basicoCadastrarUsuarioNaoValidadoSubFormDOJO = Basico_UtilControllerController::escapaCaracteresFormDialogDOJO($basicoCadastrarUsuarioNaoValidadoSubFormDOJO);
+    $basicoCadastrarDadosUsuarioDadosAcademicosMaiorTitulacaoSubFormDOJO = new Basico_Form_CadastrarDadosUsuarioDadosAcademicosMaiorTitulacao();
+    $basicoCadastrarDadosUsuarioDadosAcademicosMaiorTitulacaoSubFormDOJO = Basico_UtilControllerController::escapaCaracteresFormDialogDOJO($basicoCadastrarDadosUsuarioDadosAcademicosMaiorTitulacaoSubFormDOJO);
     $elements[0] = $this->createElement('button', 'buttonDialogDojo1');
-    $elements[0]->setAttribs(array('label' => "{$this->getView()->tradutor('FORM_FIELD_NOME_HINT')}", 'onClick' => "exibirForm(\"Basico_Form_CadastrarUsuarioNaoValidado\", \"{$basicoCadastrarUsuarioNaoValidadoSubFormDOJO}\", \"{$this->getView()->tradutor('FORM_FIELD_NOME_HINT')}\")"));
+    $elements[0]->setAttribs(array('label' => "{$this->getView()->tradutor('FORM_FIELD_MAIOR_TITULACAO')}", 'onClick' => "exibirForm(\"Basico_Form_CadastrarDadosUsuarioDadosAcademicosMaiorTitulacao\", \"{$basicoCadastrarDadosUsuarioDadosAcademicosMaiorTitulacaoSubFormDOJO}\", \"{$this->getView()->tradutor('FORM_FIELD_MAIOR_TITULACAO')}\")"));
     $elements[0]->setRequired(false);
 
     // Adicionando elementos ao formulario.
