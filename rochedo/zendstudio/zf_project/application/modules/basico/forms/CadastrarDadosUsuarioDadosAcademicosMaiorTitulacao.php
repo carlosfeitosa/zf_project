abd4a119c988aa2aa34f005e842f1d5d330875be
adriano.lemos <adriano.lemos@19d60574-38d4-4ac3-a504-0da25a219391>
2010-10-06 16:48:57 +0000

    Sprint 0003.2 - Dados Acadêmicos - Criação de formulário do Dialog de Maior titulação

---
+++ rochedo/zendstudio/zf_project/application/modules/basico/forms/CadastrarDadosUsuarioDadosAcademicosMaiorTitulacao.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Rochedo Framework
+*
+* Formulário gerado automáticamente pelo Gerador rochedo
+* em: 06/10/2010 13:46:26
+*
+* LICENÇA DE USO
+*
+* (implementar licença)
+*
+*
+* @category   RochedoProject
+* @package    BASICO
+* @copyright  Copyright (c) 2010 Rochedo Project. (http://www.rochedoproject.com)
+* @license    (implementar)
+* @version    1: 04/10/2010 16:03:35
+*/
+class Basico_Form_CadastrarDadosUsuarioDadosAcademicosMaiorTitulacao extends Zend_Dojo_Form
+{
+    /**
+    * Constructor do Form
+    * @param array $options
+    * @return Basico_Form_CadastrarDadosUsuarioDadosAcademicosMaiorTitulacao
+    */
+    public function __construct($options = null)
+    {
+        // Inicializando o formulário.
+        parent::__construct($options);
+
+        $this->setName('CadastrarDadosUsuarioDadosAcademicosMaiorTitulacao');
+        $this->setMethod('post');
+
+    }
+}
+?>
