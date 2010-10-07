63ffa892e5ff86c90100faa9fdc75c5c71a81063
carlos.feitosa <carlos.feitosa@19d60574-38d4-4ac3-a504-0da25a219391>
2010-10-07 14:32:56 +0000

    sprint 0007: - criacao dos scripts de banco de dados; - criacao de metodos para verificacao de registros filhos, atraves de categoria chave estrangeira; - criacao de contador de transacoes para nao fazer transacoes aninhadas;
    sprint 0001:
    - implementacao de metodo para verificacao de valores de chaves estrangeiras atraves de categoria chave estrangeira, relacionando as tabelas/campos de origem.

---
+++ rochedo/zendstudio/zf_project/scripts/db/mssql/schema/sprint_0007_create_schema.sql
@@ -0,0 +1,37 @@
+/*
+* SCRIPT DE CRIACAO DAS TABELAS DO SPRINT 0006
+* 
+* versao: 1.0 (MSSQL 2000)
+* por: CARLOS FEITOSA (carlos.feitosa@rochedoproject.com)
+* criacao: 05/10/2010
+*/
+
+/* CRIACAO DAS TABELAS */
+
+create table relacao_categoria_chave_estrangeira (
+	id int identity (1, 1) not null ,
+	tabela_origem varchar (100) collate latin1_general_ci_ai not null ,
+	campo_origem varchar (100) collate latin1_general_ci_ai not null ,
+	rowinfo varchar (2000) collate latin1_general_ci_ai not null 
+) on [primary];
+
+
+/* CRIACAO DAS CHAVES PRIMARIAS */
+
+alter table relacao_categoria_chave_estrangeira with nocheck add constraint pk_relacao_categoria_chave_estrangeira primary key clustered (id) on [primary];
+
+
+/* CRIACAO DOS INDICES */
+
+create index ix_relacao_categoria_chave_estrangeira_tabela_origem on relacao_categoria_chave_estrangeira (tabela_origem) on [primary];
+create index ix_relacao_categoria_chave_estrangeira_campo_origem on relacao_categoria_chave_estrangeira (campo_origem) on [primary];
+
+
+/* CRIACAO DAS CONSTRAINTS UNIQUE */
+
+alter table relacao_categoria_chave_estrangeira add
+	constraint ix_relacao_categoria_chave_estrangeira_tabela_origem_campo_origem unique nonclustered
+	(
+		tabela_origem,
+		campo_origem
+	) on [primary];
---
+++ rochedo/zendstudio/zf_project/scripts/db/mssql/schema/sprint_0007_drop_schema.sql
@@ -0,0 +1,9 @@
+/*
+* SCRIPT DE DROP DAS TABELAS DO SPRINT 0006
+* 
+* versao: 1.0 (MSSQL 2000)
+* por: CARLOS FEITOSA (carlos.feitosa@rochedoproject.com)
+* criacao: 05/10/2010
+*/
+
+drop table relacao_categoria_chave_estrangeira;
