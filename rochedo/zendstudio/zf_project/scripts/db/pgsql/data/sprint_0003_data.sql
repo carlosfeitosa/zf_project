ded4b661a7e1d336e8696a1005895d9d60e6b248
adriano.lemos <adriano.lemos@19d60574-38d4-4ac3-a504-0da25a219391>
2010-10-19 14:13:10 +0000

    Sprint 0003.2 - Dados Acadêmicos -
    - Inserção de campos no dialog de Maior titulação
    - Correções textuais

--- rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprint_0003_data.sql
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprint_0003_data.sql
@@ -74,7 +74,7 @@ WHERE t.nome = 'LINGUAGEM'
 AND c.nome = 'pt-br';
 
 INSERT INTO dicionario_expressao (id_categoria, constante_textual, traducao)
-SELECT c.id, 'FORM_FIELD_MAIOR_TITULACAO' AS constante_textual, 'Maior Titulação:' AS traducao
+SELECT c.id, 'FORM_FIELD_MAIOR_TITULACAO' AS constante_textual, 'Maior Titulação' AS traducao
 FROM tipo_categoria t
 LEFT JOIN categoria c ON (t.id = c.id_tipo_categoria)
 WHERE t.nome = 'LINGUAGEM'
