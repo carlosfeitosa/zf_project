51a8edf3f02d5374908c35c7e8f523d9abdc1d2e
joao.vasconcelos <joao.vasconcelos@19d60574-38d4-4ac3-a504-0da25a219391>
2010-10-21 19:24:15 +0000

    Distribuição dos scripts nos arquivos de cada tabela concluída.

---
+++ rochedo/zendstudio/zf_project/scripts/db/mssql/data/sprint_0001~0007_data/data_seq_0291_dados_pessoais.sql
@@ -0,0 +1,8 @@
+/*
+* SCRIPT DE POPULACAO DA TABELA DADOS_PESSOAIS
+* 
+* versao: 1.0 (MSSQL 2000)
+* por: JOÃO VASCONCELOS (joao.vasconcelos@rochedoproject.com)
+* criacao: 20/10/2010
+* ultimas modificacoes: 
+*/
---
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprint_0001~0007_data/data_seq_0391_categoria_chave_estrangeira.sql
@@ -0,0 +1,29 @@
+/*
+* SCRIPT DE POPULACAO DA TABELA CATEGORIA_CHAVE_ESTRANGEIRA
+* 
+* versao: 1.0 (POSTGRESQL 8.4.1)
+* por: JOÃO VASCONCELOS (joao.vasconcelos@rochedoproject.com)
+* criacao: 20/10/2010
+* ultimas modificacoes: 
+*/
+
+INSERT INTO categoria_chave_estrangeira (id_categoria, tabela_estrangeira, campo_estrangeiro, rowinfo)
+SELECT c.id AS id_categoria, 'pessoa' AS tabela_estrangeira, 'id' AS campo_estrangeiro, 'SYSTEM_STARTUP' AS rowinfo
+FROM tipo_categoria t
+LEFT JOIN categoria c ON (t.id = c.id_tipo_categoria)
+WHERE t.nome = 'EMAIL'
+AND c.nome = 'EMAIL_PRIMARIO';
+
+INSERT INTO categoria_chave_estrangeira (id_categoria, tabela_estrangeira, campo_estrangeiro, rowinfo)
+SELECT c.id AS id_categoria, 'email' AS tabela_estrangeira, 'id' AS campo_estrangeiro, 'SYSTEM_STARTUP' AS rowinfo
+FROM tipo_categoria t
+LEFT JOIN categoria c ON (t.id = c.id_tipo_categoria)
+WHERE t.nome = 'MENSAGEM'
+AND c.nome = 'MENSAGEM_EMAIL_VALIDACAO_USUARIO_PLAINTEXT';
+
+INSERT INTO categoria_chave_estrangeira (id_categoria, tabela_estrangeira, campo_estrangeiro, rowinfo)
+SELECT c.id AS id_categoria, 'pessoa' AS tabela_estrangeira, 'id' AS campo_estrangeiro, 'SYSTEM_STARTUP' AS rowinfo
+FROM tipo_categoria t
+LEFT JOIN categoria c ON (t.id = c.id_tipo_categoria)
+WHERE t.nome = 'SISTEMA'
+AND c.nome = 'SISTEMA_EMAIL';
