5eef38b52eeb92a6cfa6cd7b691405d00e2f5a72
carlos.feitosa <carlos.feitosa@19d60574-38d4-4ac3-a504-0da25a219391>
2011-02-04 16:43:34 +0000

    sprint 0002.1: - dicionario de expressoes (for sync).

--- rochedo/zendstudio/zf_project/scripts/db/mssql/data/sprint_0001~0007_data/data_seq_0031_dicionario_expressao.sql
+++ rochedo/zendstudio/zf_project/scripts/db/mssql/data/sprint_0001~0007_data/data_seq_0031_dicionario_expressao.sql
@@ -83,6 +83,7 @@ LEFT JOIN categoria c ON (t.id = c.id_tipo_categoria)
 WHERE t.nome = 'LINGUAGEM'
 AND c.nome = 'pt-br';
 
+INSERT INTO dicionario_expressao (id_categoria, constante_textual, traducao)
 SELECT c.id, 'FORM_ELEMENT_VALIDATOR_INDETICAL_NOT_SAME_SENHA_CONFIRMACAO' AS constante_textual, 'O valor deste campo precisa ser indêntico ao do campo senha.' AS traducao
 FROM tipo_categoria t
 LEFT JOIN categoria c ON (t.id = c.id_tipo_categoria)
@@ -2233,6 +2234,7 @@ LEFT JOIN categoria c ON (t.id = c.id_tipo_categoria)
 WHERE t.nome = 'LINGUAGEM'
 AND c.nome = 'en-us';
 
+INSERT INTO dicionario_expressao (id_categoria, constante_textual, traducao)
 SELECT c.id, 'FORM_ELEMENT_VALIDATOR_INDETICAL_NOT_SAME_SENHA_CONFIRMACAO' AS constante_textual, 'The two given passwords do not match.' AS traducao
 FROM tipo_categoria t
 LEFT JOIN categoria c ON (t.id = c.id_tipo_categoria)
