a37f5729045a8db5816953c16bfe6ef9b6df0d68
carlos.feitosa <carlos.feitosa@19d60574-38d4-4ac3-a504-0da25a219391>
2011-02-24 18:33:46 +0000

    exclusao de codigo mopado

--- rochedo/zendstudio/zf_project/scripts/db/mssql/1/data_seq_0051_estado.sql
+++
@@ -1,9 +0,0 @@
-/**
-* SCRIPT DE POPULACAO DA TABELA ESTADO
-* 
-* versao: 1.0 (MSSQL 2000)
-* por: CARLOS FEITOSA (carlos.feitosa@rochedoproject.com)
-* criacao: 20/10/2010
-* ultimas modificacoes:
-* 
-*/
--- rochedo/zendstudio/zf_project/scripts/db/mssql/1/data_seq_0341_mensagem_email.sql
+++
@@ -1,8 +0,0 @@
-/*
-* SCRIPT DE POPULACAO DA TABELA MENSAGEM_EMAIL
-* 
-* versao: 1.0 (MSSQL 2000)
-* por: JOÃO VASCONCELOS (joao.vasconcelos@rochedoproject.com)
-* criacao: 20/10/2010
-* ultimas modificacoes: 
-*/
