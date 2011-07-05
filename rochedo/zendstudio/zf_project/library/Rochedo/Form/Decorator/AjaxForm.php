e4608022fa9765135f6f69cab2f018ee38a78a74
igor.pinho.souza <igor.pinho.souza@19d60574-38d4-4ac3-a504-0da25a219391>
2011-07-05 20:48:55 +0000

    Sprint 0003.8 - implementacao do decorator ajaxForm.

--- rochedo/zendstudio/zf_project/library/Rochedo/Form/Decorator/AjaxForm.php
+++ rochedo/zendstudio/zf_project/library/Rochedo/Form/Decorator/AjaxForm.php
@@ -3,21 +3,34 @@
 class Rochedo_Form_Decorator_AjaxForm extends Zend_Form_Decorator_Abstract
 {
     private $possibleXhrArgs = array ('load','error','handle','headers','timeout','sync');
+    private $formName;
+    private $idRequestSource;
+    
+    
     public function render($content)
     {
         $form = $this->getElement();
-        if (!$form instanceof Zend_Dojo_Form) {
+        if (!$form instanceof Zend_Form) {
+        	echo 'not instanceof Zend_Form';
+        	exit;
             return $content;
         }
 
-        if (!$this->getOption('load')) {   
+        if (!$this->getOption('load')) {
+		   	//echo "getOption('load')";
+        	//exit;   
             return $content;
         }
         
         
         if (!$form->getAction()) {
             $form->setAction('.');
+            echo "no getAction()";
+        	exit;
         } 
+
+        // setando o nome do formulario no id para poder ser identific
+        $this->idRequestSource = $form->getName();
 
         $xhrPostArgs = array ( );
         
@@ -41,9 +54,20 @@ class Rochedo_Form_Decorator_AjaxForm extends Zend_Form_Decorator_Abstract
 
     protected function _renderHandler($userArgs)
     {
-        $standardArgs = array(
+    	$standardArgs = array(
+    		
+    		// content deve ser um objeto javaScript de pares nome/valor
+    		'content' => new Zend_Json_Expr("{idRequestSource: '{$this->idRequestSource}',
+    										  key1: 'value1',
+					    					  key2: 'value2',
+					    					  key3: 'value3'
+    										 }"),
+    		'testeArgs' => 'xxxxxxxxxxxxxxxxxxxx',
+    		//'postData' => 'data postado....',
+    	
             'form'     => new Zend_Json_Expr('this.domNode'),
-            'handleAs' => 'text',
+            'handleAs' => 'json',
+
         );
         
         if (is_array($userArgs))  {
@@ -56,12 +80,20 @@ class Rochedo_Form_Decorator_AjaxForm extends Zend_Form_Decorator_Abstract
 
         $content =<<<EOQ
 <script type="dojo/method" event="onSubmit">
-if (this.validate()) {
-    dojo.xhrPost($xhrArgs);
+//loading();
+validateForm('CadastrarUsuarioValidado', 'titulo', 'mensagem....', '');
+return;
+if (validateForm('CadastrarUsuarioValidado', 'titulo', 'mensagem....', '')){
+	if (this.validate()) {
+	    dojo.xhrPost($xhrArgs);
+	}
+	return false;
 }
+//loading('hide');
 return false;
 </script>
 EOQ;
+
         return $content;
     }
 }
