b9e0cb972af8e22e6cc6a9b288ac2b23a9fd46a0
carlos.feitosa <carlos.feitosa@19d60574-38d4-4ac3-a504-0da25a219391>
2011-09-29 14:41:36 +0000

    sprint 0009: - criacao da acao dialogautenticacao, para retorno do formulario de autenticacao de usuario e vinculacao desta acao com o perfil publico.

--- rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprint_0009_data/sprint_0009_data.sql
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprint_0009_data/sprint_0009_data.sql
@@ -11,7 +11,8 @@
 * 											25/07/2011 - criacao das constantes textuais para a view de host banido;
 * 											15/08/2011 - criacao das acoes e vinculacoes com o perfil de administrador e desenvolvedor da acao "regerarchecksummodelo" no controlador "administrador";
 * 											31/08/2011 - criacao das acoes e vinculacoes com o perfil de usuario validado e administrador da acao "trocarsenhaexpirada" no controlador "dadosusuario";
-* 											14/09/2011 - criacao da acao "salvarusuarionaovalidado" e vinculacao com o perfil de usuario publico
+* 											14/09/2011 - criacao da acao "salvarusuarionaovalidado" e vinculacao com o perfil de usuario publico;
+* 											29/09/2011 - criacao da acao "dialogautenticacao" e vinculacao com o perfil de usuario publico;
 */
 
 -- DICIONARIO DE EXPRESSOES
@@ -468,6 +469,11 @@ FROM modulo m
 WHERE m.nome = 'BASICO';
 
 INSERT INTO acao_aplicacao (id_modulo, controller, action, rowinfo)
+SELECT m.id AS id_modulo, 'autenticador' AS controller, 'dialogautenticacao' AS action, 'SYSTEM_STARTUP' AS rowinfo
+FROM modulo m
+WHERE m.nome = 'BASICO';
+
+INSERT INTO acao_aplicacao (id_modulo, controller, action, rowinfo)
 SELECT m.id AS id_modulo, 'controleacesso' AS controller, 'ipusuariodiferentedoipdousuarioautenticadonasessao' AS action, 'SYSTEM_STARTUP' AS rowinfo
 FROM modulo m
 WHERE m.nome = 'BASICO';
@@ -838,6 +844,19 @@ SELECT (SELECT p.id
         WHERE m.NOME = 'BASICO'
         AND a.controller = 'autenticador'
         AND a.action = 'desautenticausuario') AS id_acao_aplicacao, 'SYSTEM_STARTUP' AS rowinfo;
+
+INSERT INTO acoes_aplicacao_perfis (id_perfil, id_acao_aplicacao, rowinfo)
+SELECT (SELECT p.id
+        FROM PERFIL p
+        LEFT JOIN categoria c ON (p.id_categoria = c.id)
+        WHERE c.nome = 'PERFIL_USUARIO_SISTEMA'
+        AND p.nome   = 'USUARIO_VALIDADO') AS id_perfil,
+       (SELECT a.id
+        FROM acao_aplicacao a
+        LEFT JOIN modulo m ON (a.id_modulo = m.id)
+        WHERE m.NOME = 'BASICO'
+        AND a.controller = 'autenticador'
+        AND a.action = 'dialogautenticacao') AS id_acao_aplicacao, 'SYSTEM_STARTUP' AS rowinfo;
         
 INSERT INTO acoes_aplicacao_perfis (id_perfil, id_acao_aplicacao, rowinfo)
 SELECT (SELECT p.id
