0baaa380fdc8c6dc09d17c908e113d47f5c7c87f
joao.henrique.bione <joao.henrique.bione@19d60574-38d4-4ac3-a504-0da25a219391>
2012-02-06 11:56:35 +0000

    Redefinicao de estrutura dos scripts de insercao: - Criacao de estruturas de scripts vazios

---
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprints_0001~0013/0025_associacoes/data_seq_200020.05.basico_assoc_dados_profis.assoccl_area_conhecimento.sql
@@ -0,0 +1,9 @@
+/**
+* SCRIPT DE POPULACAO DA TABELA basico_assoc_dados_profis.assoccl_area_conhecimento
+* 
+* versao: 1.0 (POSTGRESQL 9.1.1)
+* por: JOÃO HENRIQUE M.BIONE (joao.henrique.bione@rochedoproject.com)
+* criacao: 01/02/2012
+* ultimas modificacoes:
+*
+*/
---
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprints_0001~0013/0025_associacoes/data_seq_200025.05.basico_componente.assoccl_include.sql
@@ -0,0 +1,9 @@
+/**
+* SCRIPT DE POPULACAO DA TABELA basico_componente.assoccl_include
+* 
+* versao: 1.0 (POSTGRESQL 9.1.1)
+* por: JOÃO HENRIQUE M.BIONE (joao.henrique.bione@rochedoproject.com)
+* criacao: 01/02/2012
+* ultimas modificacoes:
+*
+*/
