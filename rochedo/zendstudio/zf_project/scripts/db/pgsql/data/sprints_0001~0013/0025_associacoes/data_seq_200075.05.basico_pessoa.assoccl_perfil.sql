cdc1a7cd626495187e9eeadb66fd35090eb7be66
adriano.lemos <adriano.lemos@19d60574-38d4-4ac3-a504-0da25a219391>
2012-02-07 17:55:26 +0000

    -- Geral --
    Adaptação dos Scripts de insert de dados para o novo paradigma do banco de dados.
    
    Scripts envolvidos
    -data_seq_200075.05.basico_pessoa.assoccl_perfil.sql

---
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprints_0001~0013/0025_associacoes/data_seq_200075.05.basico_pessoa.assoccl_perfil.sql
@@ -0,0 +1,23 @@
+/**
+* SCRIPT DE POPULACAO DA TABELA basico_pessoa.assoccl_perfil
+* 
+* Esta tabela contêm os dados de associacao das tabelas
+* de basico.pessoa com basico.perfil
+* 
+* versao: 1.0 (POSTGRESQL 9.1.1)
+* por: ADRIANO LEMOS (adriano.lemos@rochedoframework.com)
+* criacao: 07/02/2012
+* ultimas modificacoes:
+* 								
+*/
+
+INSERT INTO basico_pessoa.assoccl_perfil (id_pessoa, id_perfil, ativo, rowinfo)
+SELECT (SELECT id
+		FROM basico.pessoa
+		WHERE rowinfo = 'SYSTEM_STARTUP_MASTER') AS id_pessoa, perf.id AS id_perfil, true AS ativo, 'SYSTEM_STARTUP' AS rowinfo
+FROM basico.perfil perf
+LEFT JOIN basico.categoria cat ON (perf.id_categoria = cat.id)
+LEFT JOIN basico.tipo_categoria tipo_cat ON (cat.id_tipo_categoria = tipo_cat.id)
+WHERE tipo_cat.nome = 'SISTEMA'
+AND cat.nome = 'SISTEMA_USUARIO'
+AND perf.nome = 'SISTEMA';
