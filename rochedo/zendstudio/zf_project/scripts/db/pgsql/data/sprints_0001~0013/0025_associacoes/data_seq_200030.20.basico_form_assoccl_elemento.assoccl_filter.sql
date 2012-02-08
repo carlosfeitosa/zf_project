2a8f482feef6b4eef53068941ad12dc2bc20ff8c
adriano.lemos <adriano.lemos@19d60574-38d4-4ac3-a504-0da25a219391>
2012-02-08 20:24:50 +0000

    -- Geral --
    Adaptação dos Scripts de insert de dados para o novo paradigma do banco de dados.
    
    Scripts envolvidos:
    -data_seq_200030.20.basico_form_assoccl_elemento.assoccl_filter.sql

---
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprints_0001~0013/0025_associacoes/data_seq_200030.20.basico_form_assoccl_elemento.assoccl_filter.sql
@@ -0,0 +1,12 @@
+/**
+* SCRIPT DE POPULACAO DA TABELA basico_form_assoccl_elemento.assoccl_filter
+* 
+* Esta tabela contêm os dados de associacao dos filtros
+* (não padrões) com os elementos cadastrados no sistema
+* 
+* versao: 1.0 (POSTGRESQL 9.1.1)
+* por: ADRIANO LEMOS (adriano.lemos@rochedoframework.com)
+* criacao: 07/02/2012
+* ultimas modificacoes:
+* 								
+*/
