8694df18da5f39b290c4819aeacf5ff6d028ce7b
adriano.lemos <adriano.lemos@19d60574-38d4-4ac3-a504-0da25a219391>
2012-02-08 20:29:30 +0000

    -- Geral --
    Adaptação dos Scripts de insert de dados para o novo paradigma do banco de dados.
    
    Scripts envolvidos:
    -data_seq_250005.05.basico_assoccl_pessoa_perfil.assoc_dados.sql

---
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprints_0001~0013/0030_especializacoes_associacoes/data_seq_250005.05.basico_assoccl_pessoa_perfil.assoc_dados.sql
@@ -0,0 +1,18 @@
+/**
+* SCRIPT DE POPULACAO DA TABELA basico_formulario_elemento.assoccl_validator
+* 
+* Esta tabela contêm os dados de especializacao das associacoes
+* de basico_pessoa.assoccl_perfil com a associacao de dados
+* 
+* versao: 1.0 (POSTGRESQL 9.1.1)
+* por: ADRIANO LEMOS (adriano.lemos@rochedoframework.com)
+* criacao: 07/02/2012
+* ultimas modificacoes:
+* 								
+*/
+
+INSERT into basico_assoccl_pessoa_perfil.assoc_dados (id_assoccl_pessoa_perfil, assinatura_mensagem_email, rowinfo)
+SELECT pp.id, 'Equipe ROCHEDO project' AS assinatura_mensagem_email, 'SYSTEM_STARTUP' AS rowinfo
+FROM basico_pessoa.assoccl_perfil pp
+LEFT JOIN basico.pessoa p ON (p.id = pp.id_pessoa)
+WHERE p.rowinfo = 'SYSTEM_STARTUP_MASTER';
