2eda628f833f926854e1ed7126eadb2f2758bff1
adriano.lemos <adriano.lemos@19d60574-38d4-4ac3-a504-0da25a219391>
2012-02-08 18:30:12 +0000

    -- Geral --
    Ajustes na adaptação dos Scripts de insert de dados para o novo paradigma do banco de dados.
    
    Scripts envolvidos:
    -data_seq_200045.15.basico_formulario_elemento.assoccl_validator.sql

--- rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprints_0001~0013/0025_associacoes/data_seq_200045.15.basico_formulario_elemento.assoccl_validator.sql
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprints_0001~0013/0025_associacoes/data_seq_200045.15.basico_formulario_elemento.assoccl_validator.sql
@@ -10,3 +10,258 @@
 * ultimas modificacoes:
 * 								
 */
+
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_PASSWORD_TEXT_BOX_SENHA_WITH_CHECKER') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'STRING_LENGTH_6_TO_100') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_NUMBER_TEXT_BOX_CARGA_HORARIA_SEMANAL') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'INT') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+       
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_VALIDATION_TEXT_BOX_NOME_USUARIO') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'NOT_EMPTY') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+       
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_VALIDATION_TEXT_BOX_LOGIN') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'NOT_EMPTY') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+       
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_VALIDATION_TEXT_BOX_LOGIN') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'STRING_LENGTH_3_TO_100') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+       
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_VALIDATION_TEXT_BOX_LOGIN') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'REGEX_/^[(a-zA-Z)]+[(a-zA-Z0-9_@\.)]*$/') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_DATE_TEXT_BOX_DATA_NASCIMENTO') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'NOT_EMPTY') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+      
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_PASSWORD_TEXT_BOX_SENHA_WITH_CHECKER') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'NOT_EMPTY') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+       
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_PASSWORD_TEXT_BOX_SENHA_CONFIRMACAO') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'NOT_EMPTY') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+       
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_PASSWORD_TEXT_BOX_SENHA_CONFIRMACAO') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'IDENTICAL') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+       
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_VALIDATION_TEXT_BOX_EMAIL_USUARIO') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'NOT_EMPTY') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_VALIDATION_TEXT_BOX_EMAIL_USUARIO') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'EMAIL_ADDRESS_DEEP_MX') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_PASSWORD_TEXT_BOX_CONFIRMACAO_NOVA_SENHA') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'IDENTICAL') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_PASSWORD_TEXT_BOX_CONFIRMACAO_NOVA_SENHA') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'STRING_LENGTH_6_TO_100') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
+
+INSERT INTO basico_formulario_elemento.assoccl_validator (id_elemento, id_validator, rowinfo)
+SELECT (SELECT fe.id
+        FROM basico_formulario.elemento fe
+        LEFT JOIN basico.categoria c ON (fe.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO'
+        AND fe.nome = 'FORM_FIELD_PASSWORD_TEXT_BOX_CONFIRMACAO_NOVA_SENHA') AS id_formulario_elemento,
+       (SELECT fev.id
+        FROM basico.validator fev
+        LEFT JOIN basico.categoria c ON (fev.id_categoria = c.id)
+        LEFT JOIN basico.tipo_categoria t ON (c.id_tipo_categoria = t.id)
+        WHERE t.nome = 'FORMULARIO'
+        AND c.nome = 'FORMULARIO_ELEMENTO_VALIDATOR'
+        AND fev.nome = 'NOT_EMPTY') AS id_formulario_elemento_validator,
+       'SYSTEM_STARTUP' AS rowinfo;
