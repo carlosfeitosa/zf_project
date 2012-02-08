0b31c93ea0bb65d90ce030682f82190f3fe2e017
adriano.lemos <adriano.lemos@19d60574-38d4-4ac3-a504-0da25a219391>
2012-02-08 14:35:12 +0000

    -- Geral --
    Adaptação dos Scripts de insert de dados para o novo paradigma do banco de dados.
    
    Scripts envolvidos:
    -data_seq_200080.05.basico_pessoa_juridica.assoccl_area_economia.sql
    -data_seq_200080.10.basico_pessoa_juridica.assoccl_capital_social.sql
    -data_seq_200080.15.basico_pessoa_juridica.assoccl_faturamento.sql
    -data_seq_200080.20.basico_pessoa_juridica.assoccl_quadro_funcionario.sql

---
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/data/sprints_0001~0013/0025_associacoes/data_seq_200080.10.basico_pessoa_juridica.assoccl_capital_social.sql
@@ -0,0 +1,12 @@
+/**
+* SCRIPT DE POPULACAO DA TABELA basico_pessoa_juridica.assoccl_capital_social
+* 
+* Esta tabela contêm os dados de associacao das tabelas
+* de basico.pessoa_juridica com basico.capital_social
+* 
+* versao: 1.0 (POSTGRESQL 9.1.1)
+* por: ADRIANO LEMOS (adriano.lemos@rochedoframework.com)
+* criacao: 07/02/2012
+* ultimas modificacoes:
+* 								
+*/
