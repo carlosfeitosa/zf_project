ddfc36198937fea915316fd3a8ce4286d1bf4ec5
carlos.feitosa <carlos.feitosa@19d60574-38d4-4ac3-a504-0da25a219391>
2012-03-08 19:18:32 +0000

    criacao de scripts de indices unicos parciais

--- rochedo/zendstudio/zf_project/scripts/db/pgsql/schema/sprints_0001~0013/sprints_0001~0013.01_create_partial_unique_index.sql
+++ rochedo/zendstudio/zf_project/scripts/db/pgsql/schema/sprints_0001~0013/sprints_0001~0013.01_create_partial_unique_index.sql
@@ -8,41 +8,71 @@
 * 								
 */
 
-CREATE UNIQUE INDEX unp_tipo_categoria ON basico.tipo_categoria (nome)
+CREATE UNIQUE INDEX unp_tipo_categoria_sem_pai ON basico.tipo_categoria (nome)
 WHERE id_tipo_categoria_pai IS NULL;
 
-CREATE UNIQUE INDEX unp_categoria ON basico.categoria (id_tipo_categoria, nome)
+CREATE UNIQUE INDEX unp_categoria_sem_pai ON basico.categoria (id_tipo_categoria, nome)
 WHERE id_categoria_pai IS NULL;
 
-CREATE UNIQUE INDEX unp_assoccl_metedo_validacao ON basico_acao_aplicacao.assoccl_metodo_validacao (id_acao_aplicacao, id_metodo_validacao)
+CREATE UNIQUE INDEX unp_assoccl_metedo_validacao_sem_perfil ON basico_acao_aplicacao.assoccl_metodo_validacao (id_acao_aplicacao, id_metodo_validacao)
 WHERE id_perfil IS NULL;
 
-CREATE UNIQUE INDEX unp_assoc_estado ON basico_localizacao.assoc_estado (id_pais, nome, id_categoria)
+CREATE UNIQUE INDEX unp_assoc_estado_sem_pai ON basico_localizacao.assoc_estado (id_pais, nome, id_categoria)
 WHERE id_estado_pai IS NULL;
 
-CREATE UNIQUE INDEX unp_assoc_municipio ON basico_localizacao.assoc_municipio (id_estado, nome, codigo_ddd, id_categoria)
+CREATE UNIQUE INDEX unp_assoc_municipio_sem_pai ON basico_localizacao.assoc_municipio (id_estado, nome, id_categoria)
 WHERE id_municipio_pai IS NULL;
 
-CREATE UNIQUE INDEX unp_area_conhecimento ON basico.area_conhecimento (id_categoria, nome)
+CREATE UNIQUE INDEX unp_area_conhecimento_sem_pai ON basico.area_conhecimento (id_categoria, nome)
 WHERE id_area_conhecimento_pai IS NULL;
 
-CREATE UNIQUE INDEX unp_area_economia ON basico.area_economia (id_categoria, nome)
+CREATE UNIQUE INDEX unp_area_economia_sem_pai ON basico.area_economia (id_categoria, nome)
 WHERE id_area_economia_pai IS NULL;
 
-CREATE UNIQUE INDEX unp_cpg_arquivo_nome ON basico.cpg_arquivo (id_categoria, nome)
+CREATE UNIQUE INDEX unp_cpg_arquivo__sem_nome ON basico.cpg_arquivo (id_categoria, nome)
 WHERE nome IS NOT NULL;
 
-CREATE UNIQUE INDEX unp_cpg_arquivo_uri ON basico.cpg_arquivo (uri)
+CREATE UNIQUE INDEX unp_cpg_arquivo_uri_sem_remoto ON basico.cpg_arquivo (uri)
 WHERE remoto IS false;
 
-CREATE UNIQUE INDEX unp_cpg_dados_bancarios ON basico.cpg_dados_bancarios (id_categoria, id_generico_proprietario, numero_banco, numero_agencia, numero_conta)
+CREATE UNIQUE INDEX unp_cpg_dados_bancarios_sem_numero_tipo_conta ON basico.cpg_dados_bancarios (id_categoria, id_generico_proprietario, numero_banco, numero_agencia, numero_conta)
 WHERE numero_tipo_conta IS NULL;
 
-CREATE UNIQUE INDEX unp_cpg_link ON basico.cpg_link (id_categoria, id_generico_proprietario, url)
+CREATE UNIQUE INDEX unp_cpg_link_sem_nome ON basico.cpg_link (id_categoria, id_generico_proprietario, url)
 WHERE nome IS NULL;
 
-CREATE UNIQUE INDEX unp_formulario ON basico.formulario (id_categoria, nome)
+CREATE UNIQUE INDEX unp_formulario_sem_pai ON basico.formulario (id_categoria, nome)
 WHERE id_formulario_pai IS NULL;
 
-CREATE UNIQUE INDEX unp_assocag_parceria ON basico_pessoa_juridica.assocag_parceria (id_categoria, id_pessoa_juridica, id_pessoa_juridica_parceira)
+CREATE UNIQUE INDEX unp_assoccl_elemento_sem_element_name ON basico_formulario.assoccl_elemento (id_formulario , id_elemento)
+WHERE element_name IS NULL;
+
+CREATE UNIQUE INDEX unp_assocag_parceria_sem_parceria ON basico_pessoa_juridica.assocag_parceria (id_categoria, id_pessoa_juridica, id_pessoa_juridica_parceira)
 WHERE id_assocag_parceria IS NULL;
+
+CREATE UNIQUE INDEX unp_assocag_parceria_sem_pessoa_juridica_parceira ON basico_pessoa_juridica.assocag_parceria (id_categoria, id_pessoa_juridica, id_assocag_parceria)
+WHERE id_pessoa_juridica_parceira IS NULL;
+
+CREATE UNIQUE INDEX unp_regime_trabalho_sem_pai_codigo ON basico_dados_profissionais.regime_trabalho (id_categoria , nome)
+WHERE codigo IS NULL AND id_regime_trabalho_pai IS NULL;
+
+CREATE UNIQUE INDEX unp_regime_trabalho_sem_pai ON basico_dados_profissionais.regime_trabalho (id_categoria , nome, id_regime_trabalho_pai)
+WHERE codigo IS NULL;
+
+CREATE UNIQUE INDEX unp_regime_trabalho_sem_codigo ON basico_dados_profissionais.regime_trabalho (id_categoria , nome , codigo)
+WHERE id_regime_trabalho_pai IS NULL;
+
+CREATE UNIQUE INDEX unp_regime_trabalho_codigo ON basico_dados_profissionais.regime_trabalho (id_categoria , codigo)
+WHERE codigo IS NOT NULL;
+
+CREATE UNIQUE INDEX unp_tipo_vinculo_sem_codigo ON basico_dados_profissionais.tipo_vinculo (id_categoria , nome)
+WHERE codigo IS NULL;
+
+CREATE UNIQUE INDEX unp_tipo_vinculo_codigo ON basico_dados_profissionais.tipo_vinculo (id_categoria , codigo)
+WHERE codigo IS NOT NULL;
+
+CREATE UNIQUE INDEX unp_vinculo_empregaticio_sem_codigo ON basico_dados_profissionais.vinculo_empregaticio (id_categoria , nome)
+WHERE codigo IS NULL;
+
+CREATE UNIQUE INDEX unp_vinculo_empregaticio_codigo ON basico_dados_profissionais.vinculo_empregaticio (id_categoria , codigo)
+WHERE codigo IS NOT NULL;
