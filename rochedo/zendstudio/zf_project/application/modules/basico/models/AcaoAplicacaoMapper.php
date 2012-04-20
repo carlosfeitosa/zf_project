24a3767298f2ec41beb7c6b086c10daf04aedf2c
carlos.feitosa <carlos.feitosa@19d60574-38d4-4ac3-a504-0da25a219391>
2012-04-20 14:21:30 +0000

    implementação de novo mapper

--- rochedo/zendstudio/zf_project/application/modules/basico/models/AcaoAplicacaoMapper.php
+++ rochedo/zendstudio/zf_project/application/modules/basico/models/AcaoAplicacaoMapper.php
@@ -10,6 +10,35 @@
  */
 class Basico_Model_AcaoAplicacaoMapper extends Basico_AbstractMapper_RochedoMapper implements Basico_InterfaceMapper_RochedoMapperPesquisa, Basico_InterfaceMapper_RochedoMapperPersistencia
 {
+	/**
+	 * Mapeamento da classe
+	 * 
+	 * @var Array'
+	 */
+	public $_arrayMapper = array();
+
+	/**
+	 * Constructor
+	 * 
+	 * @param  array|null $options 
+	 * 
+	 * @return void
+	 */
+	public function __construct()
+	{
+		// montando array de mapeamento
+		$this->_arrayMapper['id'] = 'id';
+		$this->_arrayMapper['idModulo'] = 'id_modulo';
+		$this->_arrayMapper['controller'] = 'controller';
+		$this->_arrayMapper['action'] = 'action';
+		$this->_arrayMapper['constanteTextual'] = 'constante_textual';
+		$this->_arrayMapper['constanteTextualDescricao'] = 'constante_textual_descricao';
+		$this->_arrayMapper['ativo'] = 'ativo';
+		$this->_arrayMapper['datahoraCriacao'] = 'datahora_criacao';
+		$this->_arrayMapper['datahoraUltimaAtualizacao'] = 'datahora_ultima_atualizacao';
+		$this->_arrayMapper['rowinfo'] = 'rowinfo'; 
+	}
+
    	/**
      * Get registered Zend_Db_Table instance
      *
@@ -19,33 +48,22 @@ class Basico_Model_AcaoAplicacaoMapper extends Basico_AbstractMapper_RochedoMapp
     */ 
     public function getDbTable($dbTable = 'Basico_Model_DbTable_AcaoAplicacao')
     {
+    	// chamando método do pai
         return parent::getDbTable($dbTable);
     }
     
 	/**
      * Find a AcaoAplicacao entry by id
      * 
-     * @param  int $id 
+     * @param  int $id
      * @param  Basico_Model_AcaoAplicacao $object 
+     * 
      * @return void
      */
     public function find($id, Basico_AbstractModel_RochedoPersistentModeloGenerico $object)
     {
-        $result = $this->getDbTable()->find($id);
-        if (0 == count($result)) {
-            return;
-        }
-        $row = $result->current();
-        $object->setId($row->id)
-				->setIdModulo($row->id_modulo)
-				->setController($row->controller)
-				->setAction($row->action)
-				->setConstanteTextual($row->constante_textual)
-				->setConstanteTextualDescricao($row->constante_textual_descricao)
-				->setAtivo($row->ativo)
-				->setDatahoraCriacao($row->datahora_criacao)
-				->setDatahoraUltimaAtualizacao($row->datahora_ultima_atualizacao)
-				->setRowinfo($row->rowinfo);
+    	// chamando método do pai
+    	return $this->findAbstrato($this->_arrayMapper, $id, $object);
     }
 
 	/**
@@ -55,26 +73,8 @@ class Basico_Model_AcaoAplicacaoMapper extends Basico_AbstractMapper_RochedoMapp
 	 */
 	public function fetchAll()
 	{
-		$resultSet = $this->getDbTable()->fetchAll();
-		$entries   = array();
-		foreach ($resultSet as $row) 
-		{
-			$entry = new Basico_Model_AcaoAplicacao();
-			$entry->setId($row->id)
-
-				->setIdModulo($row->id_modulo)
-				->setController($row->controller)
-				->setAction($row->action)
-				->setConstanteTextual($row->constante_textual)
-				->setConstanteTextualDescricao($row->constante_textual_descricao)
-				->setAtivo($row->ativo)
-				->setDatahoraCriacao($row->datahora_criacao)
-				->setDatahoraUltimaAtualizacao($row->datahora_ultima_atualizacao)
-				->setRowinfo($row->rowinfo)
-				->setMapper($this);
-			$entries[] = $entry;
-		}
-		return $entries;
+		// chamando método pai
+		return $this->fetchListAbstrato($this->_arrayMapper, 'Basico_Model_AcaoAplicacao');
 	}
 	
 	/**
@@ -84,64 +84,33 @@ class Basico_Model_AcaoAplicacaoMapper extends Basico_AbstractMapper_RochedoMapp
 	 */
 	public function fetchList($where=null, $order=null, $count=null, $offset=null)
 	{
-		$resultSet = $this->getDbTable()->fetchAll($where, $order, $count, $offset);
-		$entries   = array();
-		foreach ($resultSet as $row) 
-		{
-			$entry = new Basico_Model_AcaoAplicacao();
-			$entry->setId($row->id)
-
-				->setIdModulo($row->id_modulo)
-				->setController($row->controller)
-				->setAction($row->action)
-				->setConstanteTextual($row->constante_textual)
-				->setConstanteTextualDescricao($row->constante_textual_descricao)
-				->setAtivo($row->ativo)
-				->setDatahoraCriacao($row->datahora_criacao)
-				->setDatahoraUltimaAtualizacao($row->datahora_ultima_atualizacao)
-				->setRowinfo($row->rowinfo)
-				->setMapper($this);
-			$entries[] = $entry;
-		}
-		return $entries;
+		// chamando método pai
+		return $this->fetchListAbstrato($this->_arrayMapper, 'Basico_Model_AcaoAplicacao', $where, $order, $count, $offset);
 	}
     
     /**
      * Save a AcaoAplicacao entry
      * 
      * @param  Basico_Model_AcaoAplicacao $object
+     * 
      * @return void
      */
     public function save(Basico_AbstractModel_RochedoPersistentModeloGenerico $object)
     {
-        $data = array(
-        		'id_modulo'            		  => $object->getIdModulo(),
-				'controller'           		  => $object->getController(),
-				'action'               		  => $object->getAction(),
-				'constante_textual'    		  => $object->getConstanteTextual(),
-        		'constante_textual_descricao' => $object->getConstanteTextualDescricao(),
-        		'ativo'                	 	  => $object->getAtivo(),
-				'datahora_criacao' 		  	  => $object->getDatahoraCriacao(),
-				'datahora_ultima_atualizacao' => $object->getDatahoraUltimaAtualizacao(),
-                'rowinfo'              		  => $object->getRowinfo(),
-
-        );
-
-        if (null === ($id = $object->getId())) {
-            unset($data['id']);
-            $object->setId($this->getDbTable()->insert($data));
-        } else {
-            $this->getDbTable()->update($data, array('id = ?' => $id));
-        }
+    	// chamando método pai
+    	return $this->saveAbstrato($this->_arrayMapper, $object);
     }
     
 	/**
 	* Delete a AcaoAplicacao entry
+	* 
 	* @param Basico_Model_AcaoAplicacao $object
+	* 
 	* @return void
 	*/
 	public function delete(Basico_AbstractModel_RochedoPersistentModeloGenerico $object)
 	{
-    	$this->getDbTable()->delete(array('id = ?' => $object->id));
+		// chamando método pai
+    	$this->deleteAbstrato($this->_arrayMapper, $object);
 	}
 }
