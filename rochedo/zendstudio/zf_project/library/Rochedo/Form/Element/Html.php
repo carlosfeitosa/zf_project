092019bc574acbb945e46ce1fe31ae55f7e75d4f
joao.vasconcelos <joao.vasconcelos@19d60574-38d4-4ac3-a504-0da25a219391>
2012-10-17 20:25:43 +0000

    - Criacao do metodo para override no metodo isValid para o elemento Rochedo_Html;

--- rochedo/zendstudio/zf_project/library/Rochedo/Form/Element/Html.php
+++ rochedo/zendstudio/zf_project/library/Rochedo/Form/Element/Html.php
@@ -14,4 +14,23 @@ class Rochedo_Form_Element_Html extends Zend_Form_Element_Xhtml
      * @var string
      */
     public $helper = 'formNote';
+    
+    /**
+     * Validate element value
+     *
+     * If a translation adapter is registered, any error messages will be
+     * translated according to the current locale, using the given error code;
+     * if no matching translation is found, the original message will be
+     * utilized.
+     *
+     * Note: The *filtered* value is validated.
+     *
+     * @param  mixed $value
+     * @param  mixed $context
+     * @return boolean
+     */
+    public function isValid($value, $context = null)
+    {
+    	return true;
+    }
 }
